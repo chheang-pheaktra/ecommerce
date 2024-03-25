932261e27d91c54981924482930dedce0ee757cc
chhornsreyneat <chhornsreyneat96@gmail.com>
2024-03-25 15:39:38 +0700

    update homepage

--- app/src/main/java/kh/edu/rupp/ecommerce/MainActivity.java
+++ app/src/main/java/kh/edu/rupp/ecommerce/MainActivity.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.appcompat.widget.SearchView;
-import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
     private View searchView;
 
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.homepage_main);
         // Search
         SearchView searchView;
 
